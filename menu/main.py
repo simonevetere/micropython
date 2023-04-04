eb0611af697552899e18c1a16068c42f6b696d3b
simonevetere <simoneveterex@gmail.com>
2023-04-04 16:16:29 +0200

    finish menu

--- menu/main.py
+++ menu/main.py
@@ -1,10 +1,16 @@
 import display
 import touch
 
-def display_text(text):
-  start = 50
+idx = 0
+all_app = ""
 
-  for app in text:
+def display_text(in_app):
+  global all_app
+  start = 50
+  all_app = in_app
+  for app in in_app:
+    if all_app[idx] == app:
+      app = ">" + app
     display.text(app,150,start,0xffffff)
     start = start + 50   
   
@@ -12,6 +18,15 @@ def display_text(text):
 
 
 def onclick(button):
+  global idx
   print('app=menu&server=get_app&callback=display_text')
+  
+  if button == "B":
+    maxlen = len(all_app)
+    if idx > maxlen - 1:
+    	idx = 0
+    print('app=' + all_app[idx] + '&server=&callback=display_text')
+  if button == "A":
+    idx = idx + 1
 
 touch.callback(touch.BOTH, onclick)
--- menu/server.py
+++ menu/server.py
@@ -4,4 +4,8 @@ def get_app():
 	path = "micropython/"
 	dir_list = os.listdir(path)
 
+	dir_list.remove('menu')
+	dir_list.remove('.git')
+	dir_list.remove('.gitignore')
+
 	return str(dir_list)
