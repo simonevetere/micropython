1dece6f2a907db5d53a6185a3b9c68b5ef061e8c
simonevetere <simoneveterex@gmail.com>
2023-04-04 15:53:32 +0200

    Update main.py

--- menu/main.py
+++ menu/main.py
@@ -2,16 +2,16 @@ import display
 import touch
 
 def display_text(text):
-	start = 50
+  start = 50
 
-	for app in text:
-		display.text(app,150,start,0xffffff)
-		start = start + 50   
-	
-	display.show()
+  for app in text:
+    display.text(app,150,start,0xffffff)
+    start = start + 50   
+  
+  display.show()
 
 
 def onclick(button):
-	print('app=menu&server=get_app&callback=display_text')
+  print('app=menu&server=get_app&callback=display_text')
 
 touch.callback(touch.BOTH, onclick)
